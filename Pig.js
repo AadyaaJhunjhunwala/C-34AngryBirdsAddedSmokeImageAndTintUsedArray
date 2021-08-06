75040398af10564d58d9aafb89dad5c4b3b8cb07
AadyaaJhunjhunwala <aadyaarox999@gmail.com>
2021-08-06 18:58:39 +0530

    Added smoke image and tint also used array

--- Pig.js
+++ Pig.js
@@ -13,12 +13,12 @@ if((this.body.speed)<3)
 {
 World.remove(world,this.body)  
 
-//push();
-//this.Visiblity=this.Visiblity-5;
-//tint(255,this.Visiblity)
+push();
+this.Visiblity=this.Visiblity-5;
+tint(255,this.Visiblity)
 
 //do nothing
-//image(this.image,this.body.position.x,this.body.position.y,50,50)
-//pop();
+image(this.image,this.body.position.x,this.body.position.y,50,50)
+pop();
 }}
 };
